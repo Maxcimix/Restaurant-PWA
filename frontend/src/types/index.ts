ee574ae772305c7b5a692892e42c80ad7bf0b237
Maxcimix <maxcimix@gmail.com>
2026-05-14 22:09:34 -0500

    Cambios actuales - se crea rama para hacer pruebas

--- frontend/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-// frontend/src/types/index.ts
-// Re-exporta todos los tipos para imports limpios:
-// import type { MenuItem, Order, CartItem } from '../types';
-
-export type * from './menu';
-export type * from './order';
-export type * from './table'; // ← NUEVO: Table, TableStatus, WaiterOrderPayload, etc.
